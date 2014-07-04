12f6f1bb12115b4901dba1e62bc51a59c0a95a4c
Nabil Shuhaiber <nabil@shuhaiber.com>
2014-07-04 23:20:35 +0100

    Initial commit

---
+++ StringSimilarity/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FuzzyStrings;
+
+namespace StringSimilarity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<string>
+                {
+                    "Book",
+                    "Johnson",
+                    "Jensen",
+                    "Johansen",
+                    "Johanneson",
+                    "P&L test 1",
+                    "P&L test 2",
+                    "P&L test 3",
+                    "Benchmark",
+                    "Book Id"
+                };
+
+            string input = "booc";
+            var results = list.Select(l => new {Item = l, Distance = l.DiceCoefficient(input)})
+                .Where(l => l.Distance > 0)
+                .OrderByDescending(i => i.Distance);
+
+            foreach (var result in results)
+            {
+                Console.WriteLine(result.Distance + ": " + result.Item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
